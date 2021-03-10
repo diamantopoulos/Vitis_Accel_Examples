f6b67b83617f2605b7d361cc6b5bf60e64f6cd2b
Dionysios Diamantopoulos @ vm on laptop <did@zurich.ibm.com>
2021-03-11 00:37:11 +0100

    Added compile_host_sw_emu.sh

---
+++ library_examples/MCEuropeanEngine/compile_host_sw_emu.sh
@@ -0,0 +1,7 @@
+
+
+LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu/
+make clean
+PLATFORM_REPO_PATHS=/home/diaman/projects/aws-fpga/Vitis/aws_platform/xilinx_aws-vu9p-f1_shell-v04261818_201920_2/ make host DEVICE=xilinx_aws-vu9p-f1_shell-v04261818_201920_2 TARGET=sw_emu
+
+
